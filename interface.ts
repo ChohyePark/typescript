ee98acab7bd38040eb35f671e12ef3a16fe91ae9
park cho hye <chohye0831@gmail.com>
2025-02-25 08:22:00 +0900

    인터페이스 2

--- interface.ts
+++ interface.ts
@@ -80,3 +80,52 @@ var ironman2:Developer3 = {
   skills : 'react',
   power : true
 }
+
+// 인터페이스를 이용한 인덱싱 타입 정의
+// 인덱싱이란 객체의 특정 속성을 접근하거나 배열의 인덱스로 특정 요소에 접근하는 동작을 의미
+
+interface StringArray {
+  [index:number]:string;
+}
+
+// StringArray 인터페이스는 index 속성을 정의하고, index 속성은 number 타입을 가지며, string 타입을 반환한다.
+
+var companies : StringArray = ['apple', 'microsoft', 'google'];
+
+// 객체 인덱싱 타입 정의
+
+interface SalaryMap {
+  [lavel:string]:number;
+}
+
+// SalaryMap 인터페이스는 속성 이름이 문자열 타입이고 속성 값이 숫자 타입인 모든 속성 이름/속성 값 쌍을 허용하겠다는 의미
+
+var salary:SalaryMap = {
+  'junior' : 1000,
+}
+
+// 인덱스 시그니처란 정확히 속성 이름을 명시하지 않고 속성 이름의 타입과 속성 값의 타입을 정의하는 문법
+
+interface SalaryInfo {
+  [lavel:string]:string;
+}
+
+var salary2:SalaryInfo = {
+  junior : '1000원',
+  mid : '400원',
+  senior : '1000원'
+}
+
+// 언제 사용할까? 만약 User라는 인터페이스에 id와 name 속성이 무조건 들어간다고 한다면 다음과 같이 섞어서 정의할 수도 있다.
+
+interface User {
+  id: string;
+  name: string;
+ [property:string]:string;
+}
+
+var seho:User = {
+  id : '1',
+  name : 'seho',
+  address : 'seoul'
+} 
